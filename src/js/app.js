2c0126e6b35d9fe99782f50b17449c11471fa7e0
pauchok-sk <pauchoooooook@gmail.com>
2025-01-01 19:04:03 +0300

    intro start

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import * as functions from "./files/functions.js";
 
 import "../scss/style.scss";
 import spoller from "./files/spoller.js";
+import slider from "./files/sliders.js";
 
 functions.isWebp();
 spoller();
+slider();
