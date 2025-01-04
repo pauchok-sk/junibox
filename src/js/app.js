2f0a2c35d7d75f5f1af818b46b1f6f3ee4ba70e2
pauchok-sk <pauchoooooook@gmail.com>
2025-01-04 19:13:21 +0300

    adv start

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,11 @@ import spoller from "./files/spoller.js";
 import slider from "./files/sliders.js";
 import dropCatalog from "./files/dropCatalog.js";
 import fancy from "./files/fancy.js";
+import sectionsToggle from "./files/sectionsToggle.js";
 
 functions.mediaAdaptive();
 spoller();
 slider();
 dropCatalog();
 fancy();
+sectionsToggle();
---
+++ src/js/files/sectionsToggle.js
@@ -0,0 +1,29 @@
+export default function sectionsToggle() {
+  const btnOpen = document.querySelector("#sections-open");
+  const sections = document.querySelector("#sections");
+
+  if (sections) {
+    btnOpen.addEventListener("click", (e) => {
+      e.stopPropagation();
+
+      sectionsOpen();
+    })
+
+    function sectionsOpen() {
+      document.body.classList.add("hidden");
+      sections.classList.add("_open");
+
+      const sectionsBody = sections.querySelector(".sections__body");
+
+      sectionsBody.addEventListener("click", (e) => e.stopPropagation());
+      document.body.addEventListener("click", sectionsClose);
+    }
+
+    function sectionsClose() {
+      document.body.classList.remove("hidden");
+      sections.classList.remove("_open");
+
+      document.body.removeEventListener("click", sectionsClose);
+    }
+  }
+}
