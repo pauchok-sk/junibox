229412e2aa0bec1cf4049a47665b9b3720c8c2e5
pauchok-sk <pauchoooooook@gmail.com>
2025-01-14 01:44:35 +0300

    range middle

--- src/js/files/ranges.js
+++ src/js/files/ranges.js
@@ -10,9 +10,18 @@ export default function ranges() {
         .closest(".selection-simple__wrapper-range")
         .querySelectorAll(".selection-simple__input-radio");
 
-      radios[0].checked = true;
+      const checkedRadioIndex =
+        Array.from(radios).findIndex((item) => item.checked === true) + 1;
+      const offset = radios.length === 4 ? 12.5 : 16.5;
 
-      currentSlider.style.left = `${range.value}%`;
+      if (checkedRadioIndex > 0) {
+        const percent = checkedRadioIndex * (100 / radios.length);
+
+        currentSlider.style.left = `${percent - offset}%`;
+      } else {
+        radios[0].checked = true;
+        currentSlider.style.left = `${range.value}%`;
+      }
 
       range.addEventListener("input", (e) => {
         const value = +e.target.value;
@@ -25,16 +34,27 @@ export default function ranges() {
         if (radios.length === 4) {
           if (value <= 25) {
             radios[0].checked = true;
-            offsetSliderChange(25 / 2);
+            offsetSliderChange(offset);
           } else if (value <= 50) {
             radios[1].checked = true;
-            offsetSliderChange(50 - 25 / 2);
+            offsetSliderChange(50 - offset);
           } else if (value <= 75) {
             radios[2].checked = true;
-            offsetSliderChange(75 - 25 / 2);
+            offsetSliderChange(75 - offset);
           } else if (value <= 100) {
             radios[3].checked = true;
-            offsetSliderChange(100 - 25 / 2);
+            offsetSliderChange(100 - offset);
+          }
+        } else if (radios.length === 3) {
+          if (value <= 33) {
+            radios[0].checked = true;
+            offsetSliderChange(offset);
+          } else if (value <= 66) {
+            radios[1].checked = true;
+            offsetSliderChange(66 - offset);
+          } else if (value > 66) {
+            radios[2].checked = true;
+            offsetSliderChange(99 - offset);
           }
         }
       });
